869b2f97c941d1d4adce06ab995fab9dc47e5500
hichamialaouichaymae <hichamialaouichaymae@gmail.com>
2026-03-01 13:08:43 +0100

    initial commit

---
+++ src/main/java/com/ecommerce/monolith/product/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.monolith.product.repository;
+
+import com.ecommerce.monolith.product.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
